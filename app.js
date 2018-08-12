a20c61447d5e51f3947d7eaeafb9ea11ff9f50ef
Baggio Shehadi <k3nh1n@yahoo.com>
2018-08-11 19:39:37 -0500

    added multipages

--- app.js
+++ app.js
@@ -2,19 +2,61 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
-
+fs.writeFile("jobs.csv","",function(err){
+    if (err) throw err;
+});
 axios.get("https://www.indeed.jobs/career/Home#locations").then(function(res) {
     let $ = cheerio.load(res.data);
     let cvs = "";
     $(".mosaicImageLinksItem").each(function(i,e){
-        console.log(i);
+        let cvs
+        // console.log(i);
         let href = e.attribs.href;
         let span = $(this).children("span").text();
-        console.log(`href: ${href} span: ${span}`);
+        // console.log(`href: ${href} span: ${span}`);
         axios.get(href).then(function(res){
             let $ = cheerio.load(res.data);
+            function parseIT(nextHref){
+                console.log(nextHref);
+                axios.get(nextHref).then(function(res){
+                    let $ = cheerio.load(res.data);
+                    nextHref = $(".listPagination").children("a:contains('Next')").attr("href")
+                    $(".listSingleColumnItem").each(function(index,element){
+                        let href = $(this).children().children().attr("href");
+                        let text = $(this).children().children().text().trim();
+                        text = text.replace(/\s+/g," ")
+                        text = text.replace(/,/g," ")
+                        let title = text.match(/^.+-/g);
+                        title = title[0]
+                        let location = text.replace(/^.+-/g,"");
+                        title = title.trim()
+                        title = title.slice(0,-1);
+                        title = title.trim()
+                        // console.log("original: "+text);
+                        // console.log("title: "+title);
+                        // console.log("location: "+location);
+                        // console.log("href: "+href)
+                        cvs = `${title}, ${href}, ${location}\n`
+                        // console.log(cvs);
+                        fs.appendFile("jobs.csv",cvs,function(err){
+                            if (err) throw err
+                        });
+                    });
+                    if($(".listPagination").children("a:contains('Next')").attr("href")!=null){
+                        parseIT(nextHref)
+                    }else{
+                        console.log("Done");
+                    }
+                })
+            }
+            console.log($(".listPagination").children().length>0)
+            if($(".listPagination").children().length>0){
+                let nextHref = $(".listPagination").children("a:contains('Next')").attr("href")
+                parseIT(nextHref)
+            }else{
+                //console.log("Done");
+            }
             $(".listSingleColumnItem").each(function(index,element){
-                let cvs;
                 let href = $(this).children().children().attr("href");
                 let text = $(this).children().children().text().trim();
                 text = text.replace(/\s+/g," ")
